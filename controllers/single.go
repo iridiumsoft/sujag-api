e798728757803c0e95a84c63b2f73884208d4831
Sameer Ali <info@iridiumsoft.org>
2018-05-22 02:59:30 +0500

    routes changes

--- controllers/single.go
+++ controllers/single.go
@@ -15,17 +15,17 @@ func (c *Controllers) getSinglePost(ctx *gin.Context) {
 	Slug := ctx.Param("slug")
 
 	c.App.DB.C("posts").Find(bson.M{"slug": Slug}).Select(bson.M{
-		"title":        true,
-		"content":      true,
-		"thumbnail":    true,
-		"type":         true,
-		"published_on": true,
-		"district":     true,
-		"liked":        true,
-		"author":       true,
+		"title":        1,
+		"content":      1,
+		"thumbnail":    1,
+		"type":         1,
+		"published_on": 1,
+		"district":     1,
+		"liked":        1,
+		"author":       1,
 	}).One(&Post)
 
-	c.App.DB.C("authors").Find(bson.M{"username": Post.Author}).Select(bson.M{"info": true, "name": true, "username": true, "dp_lg": true}).One(&Author)
+	c.App.DB.C("authors").Find(bson.M{"username": Post.Author}).Select(bson.M{"info": 1, "name": 1, "username": 1, "dp_lg": 1}).One(&Author)
 
 	ctx.JSON(http.StatusOK, bson.M{
 		"post":   Post,
