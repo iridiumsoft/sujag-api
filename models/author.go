10deb056682f52afd4c62ba04b890a27018e27e3
Sameer Ali <info@iridiumsoft.org>
2018-05-22 02:51:20 +0500

    routes changes

--- models/author.go
+++ models/author.go
@@ -6,6 +6,8 @@ type Author struct {
 	Id       int    `json:"_id,omitempty"`
 	Name     int    `json:"name,omitempty"`
 	UrduName int    `json:"urdu_name,omitempty"`
+	UserName int    `json:"user_name,omitempty"`
+	Info     int    `json:"info,omitempty"`
 	Dp       string `json:"dp,omitempty"`
 	Dp_lg    string `json:"dp_lg,omitempty"`
 	Type     string `json:"type,omitempty"`
