c48e6b5650506607c7030af7248c9e0eea117b7b
Sameer Ali <info@iridiumsoft.org>
2018-05-22 01:44:52 +0500

    routes changes

--- controllers/main.go
+++ controllers/main.go
@@ -28,7 +28,7 @@ func initGin() *gin.Engine {
 	g.Use(security)
 	g.Use(gin.Recovery())
 	g.Use(cors.Middleware(cors.Config{
-		Origins:        "*",
+		Origins:        "http://sujag.org, http://localhost:4200",
 		Methods:        allowedMethods,
 		RequestHeaders: "Origin, Content-Type, Access-Control-Allow-Origin",
 		ExposedHeaders: "",
