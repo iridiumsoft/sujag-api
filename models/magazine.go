c0b074bb74a8823536b9e0af3e7fe6bc549786b7
Sameer Ali <info@iridiumsoft.org>
2018-05-24 04:36:31 +0500

    main page footer code is adde

--- models/magazine.go
+++ models/magazine.go
@@ -3,5 +3,6 @@ package models
 type Magazine struct {
 	Title     string  `json:"title,omitempty"`
 	Is        string  `json:"is,omitempty"`
+	Img       string  `json:"img,omitempty"`
 	CreatedOn float64 `json:"created_on,omitempty"`
 }
